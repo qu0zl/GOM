2f14032879dc2276372d48db53848d63731eac39
greg <greg@gregfarrell.org>
2012-12-21 11:15:02 -0500

    Add bomb weapon class. Limit fighters to this for main weapons and prevent others taking these

--- apps/gom/models.py
+++ apps/gom/models.py
@@ -176,7 +176,8 @@ WEAPON_TYPE_CHOICES = (
     (8, _('Medium')),
     (9, _('Heavy')),
     (10, _('Assault')),
-    (11, _('Artillery'))
+    (11, _('Artillery')),
+    (12, _('Bomb'))
 )
 
 MOUNT_TYPE_CHOICES = (
@@ -826,12 +827,12 @@ class UnitForm(forms.ModelForm):
     basicWeapons = forms.ModelChoiceField(queryset=Weapons.objects.filter(weaponType__gte=1, weaponType__lte=2), required=False)
     SAWeapons = forms.ModelChoiceField(queryset=Weapons.objects.filter(weaponType__in=[1,2,4]), required=False)
     SpecWeapons = forms.ModelChoiceField(queryset=Weapons.objects.filter(weaponType__in=[1,2,4,5]), required=False)
-    mainWeapons1 = WeaponChoiceField(queryset=Weapons.objects.filter(weaponType__gte=4, weaponType__lte=11).order_by('weaponSize'), required=False, label=_('Main Weapons'), widget=WeaponSelect)
-    mainWeapons2 = WeaponChoiceField(queryset=Weapons.objects.filter(weaponType__gte=4, weaponType__lte=11).order_by('weaponSize'), required=False, widget=WeaponSelect)
-    mainWeapons3 = WeaponChoiceField(queryset=Weapons.objects.filter(weaponType__gte=4, weaponType__lte=11).order_by('weaponSize'), required=False, widget=WeaponSelect)
-    mainWeapons4 = WeaponChoiceField(queryset=Weapons.objects.filter(weaponType__gte=4, weaponType__lte=11).order_by('weaponSize'), required=False, widget=WeaponSelect)
-    mainWeapons5 = WeaponChoiceField(queryset=Weapons.objects.filter(weaponType__gte=4, weaponType__lte=11).order_by('weaponSize'), required=False, widget=WeaponSelect)
-    mainWeapons6 = WeaponChoiceField(queryset=Weapons.objects.filter(weaponType__gte=4, weaponType__lte=11).order_by('weaponSize'), required=False, widget=WeaponSelect)
+    mainWeapons1 = WeaponChoiceField(queryset=Weapons.objects.filter(weaponType__gte=4, weaponType__lte=12).order_by('weaponSize'), required=False, label=_('Main Weapons'), widget=WeaponSelect)
+    mainWeapons2 = WeaponChoiceField(queryset=Weapons.objects.filter(weaponType__gte=4, weaponType__lte=12).order_by('weaponSize'), required=False, widget=WeaponSelect)
+    mainWeapons3 = WeaponChoiceField(queryset=Weapons.objects.filter(weaponType__gte=4, weaponType__lte=12).order_by('weaponSize'), required=False, widget=WeaponSelect)
+    mainWeapons4 = WeaponChoiceField(queryset=Weapons.objects.filter(weaponType__gte=4, weaponType__lte=12).order_by('weaponSize'), required=False, widget=WeaponSelect)
+    mainWeapons5 = WeaponChoiceField(queryset=Weapons.objects.filter(weaponType__gte=4, weaponType__lte=12).order_by('weaponSize'), required=False, widget=WeaponSelect)
+    mainWeapons6 = WeaponChoiceField(queryset=Weapons.objects.filter(weaponType__gte=4, weaponType__lte=12).order_by('weaponSize'), required=False, widget=WeaponSelect)
     AIWeapons = WeaponChoiceField(queryset=Weapons.objects.filter(weaponType__gte=4, weaponType__lte=6).order_by('weaponSize'), required=False, label=_("Anti Infantry"), widget=WeaponSelect)
     AIWeapons2 = WeaponChoiceField(queryset=Weapons.objects.filter(weaponType__gte=4, weaponType__lte=6).order_by('weaponSize'), required=False, widget=WeaponSelect)
     inlineWeapons = forms.ModelChoiceField(queryset=Weapons.objects.filter(weaponType__in=[1,2,4]), required=False)
